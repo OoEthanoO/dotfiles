74cbe427845f8129a986219a3f550a38bc7b9bb5
Ethan Xu <ethanxucoder@gmail.com>
2025-07-29 19:43:00 -0400

    Initial commit

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotage = { enable = true },
+	    ensure_installed = {
+		"lua",
+		"cpp",
+		"python",
+	    },
+	    auto_install = false,
+	})
+    end
+    -- Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+}
